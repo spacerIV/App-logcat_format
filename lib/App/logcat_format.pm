30f8e3cb9b0975b911b62051cd1970bb14b26827
Victor Houston <victorhouston@gmail.com>
2014-03-02 12:56:26 +0000

    cleanup

--- lib/App/logcat_format.pm
+++ lib/App/logcat_format.pm
@@ -17,24 +17,24 @@ use IO::Interactive qw( is_interactive );
 
 App::logcat_format 
 
-=head1 SYNOPSIS
-
-If only one of emulator or device is running, and for the default logcat output run..
-% logcat_format 
-
-For default logcat output for emulator only ..
-% logcat_format -e for emulator
-
-For default logcat output for device only ..
-% logcat_format -d for device 
-
-For other adb logcat commands, just pipe into logcat_format ..
-% adb logcat -v threadtime | logcat_format
-% adb -e logcat -v process | logcat_format
-
 =head1 DESCRIPTION
 
-Formats the output of android 'adb logcat' command
+A tool to pretty print the output of the android sdk 'adb logcat' command.
+
+=head1 SYNOPSIS
+
+Default adb logcat pretty print ..
+    % logcat_format 
+
+For default logcat output for emulator only ..
+    % logcat_format -e 
+
+For default logcat output for device only ..
+    % logcat_format -d 
+
+For other adb logcat commands, just pipe into logcat_format ..
+    % adb logcat -v threadtime | logcat_format
+    % adb -e logcat -v process | logcat_format
 
 =head1 VERSION
 
@@ -78,7 +78,6 @@ my ( $wchar, $hchar, $wpixels, $hpixels ) = GetTerminalSize();
 
 my %longline;
 
-# do it
 sub run
 {
     my $class = shift;
@@ -93,7 +92,6 @@ sub run
         $argument = '-d' if $opt->device;
        
         my $process = IO::Async::Process->new(
-            ##command => [ 'adb', 'logcat', $argument ],
            command => [ 'adb', $argument, 'logcat' ],
            stdout => {
               on_read => sub {
